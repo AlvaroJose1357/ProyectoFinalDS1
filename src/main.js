833e1ba9f01b0e531429251b2c8d12912a0533db
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-01-29 23:38:26 -0500

    Update

---
+++ src/main.js
@@ -0,0 +1,28 @@
+/*Conexion a la base de datos*/
+const mysql = require("mysql")//mirar si son simples?
+
+const conection = mysql.createConnection({
+    host: "localhost:3000", 
+    user: "root",
+    password: "", 
+    databases:"/*falta base datos*/"
+})
+
+conection.connect((err) =>{
+    if(err) throw err
+    console.log("la conexion ha funcionado")
+})
+/*Apartir de aqui se comienzan el programa*/
+
+
+
+
+
+
+
+
+
+
+
+/*Para finalizar la conexion y termine*/
+conection.end()
