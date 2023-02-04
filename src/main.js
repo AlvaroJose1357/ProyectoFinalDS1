b8295a201c75560815de500cdcc014a27c7a9dc4
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-02-04 18:56:15 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const {engine} = require('express-handlebars');
-const myconnection = require('express-myconnection ');
-
-const bodyParser =require('body-parser');
-const mysql = require('mysql');
-
 const app = express();
-app.set('port', 4000);
 
-app.listen(app.get('port'), ()=>{
-    console.log('listen on port', app.get('port'));
-});
+app.listen(3000, ()=>{
+    console.log('server on port 3000')
+})
+
+
+
