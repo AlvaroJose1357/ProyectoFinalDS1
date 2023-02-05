41277b59ec961006175ccb18353e2a140e3e8a36
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-02-04 21:20:39 -0500

    update

--- src/customerController.js
+++ src/customerController.js
@@ -1,12 +1,21 @@
 const controller = {};
 
-
+/*
 controller.list = (req,res)=>{
-    req.getConnection (err,conn) =>{
-        conn.query('select * from mascotas');
+    req.getConnection {(err,conn) => {
+        conn.query('select * from mascotas', (err, mascotas )=> {
+            if(err){
+                res.json(err)
+            }
+            console.log(mascotas);
+            res.render('mascotas');
+        }
     }
 }
-
+*/
+controller.list = (req,res)=>{
+    req.getConnection
+}
 
 
 
