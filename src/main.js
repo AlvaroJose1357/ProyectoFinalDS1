1f28f8d568ea4fea3c1efa5f781c756f264fc624
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-02-04 21:27:41 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+//settings 
+app.set('port', process.env.PORT || 3000)
+
+
+//levantamos el servidor 
 app.listen(3000, ()=>{
     console.log('server on port 3000')
 })
