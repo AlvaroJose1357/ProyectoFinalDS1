f28e5198722a576c409fb8ee7b219e04c85e3556
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-02-04 21:08:05 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const { dirname } = require('path');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myConnection');
 const app = express();
 
 //settings 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
+//configuracion de las plantillas
+app.set('view engine', 'ejs');
+app.set('src', path.join(__dirname, 'src'));
 
+//middlewares
+
+app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+    host: 'localhost', 
+    user: 'root',
+    password: '', 
+    port: 3306, 
+    database: 
+}))
+
+//router
 
 //levantamos el servidor 
 app.listen(3000, ()=>{
-    console.log('server on port 3000')
+    console.log('server on port 3000');
 })
 
 
